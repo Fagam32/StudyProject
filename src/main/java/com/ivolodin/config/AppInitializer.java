952c65207fd2d12120ca3902e61cb70b2d042d22
ivolodin <ilya18081@gmail.com>
2020-07-13 03:48:01 +0300

    Init commit

---
+++ src/main/java/com/ivolodin/config/AppInitializer.java
@@ -0,0 +1,27 @@
+package com.ivolodin.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
+public class AppInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) {
+        AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+        rootContext.register(WebConfiguration.class);
+
+        // Manage the lifecycle of the root application context
+        servletContext.addListener(new ContextLoaderListener(rootContext));
+
+        // Register and map the dispatcher servlet
+        ServletRegistration.Dynamic dispatcher =
+                servletContext.addServlet("dispatcher", new DispatcherServlet(rootContext));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+    }
+}
---
+++ src/main/java/com/ivolodin/config/WebConfiguration.java
@@ -0,0 +1,23 @@
+package com.ivolodin.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.ivolodin", "com.ivolodin.controller"})
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Bean
+    ViewResolver viewResolver(){
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/pages/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
