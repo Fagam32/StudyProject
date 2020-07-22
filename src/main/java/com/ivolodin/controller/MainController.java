46a1831c4a0a4c2eef3922a06aaa19c21e53bed3
ivolodin <ilya18081@gmail.com>
2020-07-22 19:14:17 +0300

    Moved mappings connected with stations and edges from MainController to StationController

--- src/main/java/com/ivolodin/controller/MainController.java
+++ src/main/java/com/ivolodin/controller/MainController.java
@@ -1,74 +1,16 @@
 package com.ivolodin.controller;
 
-import com.ivolodin.entities.Station;
-import com.ivolodin.entities.StationConnect;
-import com.ivolodin.service.StationService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.View;
-
-import java.util.List;
 
 @Controller
 public class MainController {
 
 
-    private final StationService stationService;
-
-    @Autowired
-    public MainController(StationService stationService) {
-        this.stationService = stationService;
-    }
-
     @GetMapping("/")
-    public ModelAndView main(){
+    public ModelAndView main() {
         return new ModelAndView("index");
     }
 
-    @GetMapping("/addStation")
-    public ModelAndView showStationList() {
-
-        ModelAndView modelAndView = new ModelAndView("addStation");
-        List<Station> allStations = stationService.getAllStations();
-        modelAndView.addObject("stationList", allStations);
-
-        return modelAndView;
-    }
-
-    @PostMapping("/addStation")
-    public ModelAndView addStation(@RequestParam(name = "stationName") String stationName) {
-        stationService.addStation(stationName);
-
-        ModelAndView modelAndView = new ModelAndView("addStation");
-        List<Station> allStations = stationService.getAllStations();
-        modelAndView.addObject("stationList", allStations);
-
-        return modelAndView;
-    }
-
-    @GetMapping("/addEdge")
-    public ModelAndView showEdgeList() {
-        ModelAndView modelAndView = new ModelAndView("addEdge");
-        List<StationConnect> allEdges = stationService.getAllEdges();
-        modelAndView.addObject("edgeList", allEdges);
-        return modelAndView;
-    }
-
-    @PostMapping("/addEdge")
-    public ModelAndView addEdge(@RequestParam(name = "stationName1") String stationName1,
-                                @RequestParam(name = "stationName2") String stationName2,
-                                @RequestParam(name = "distance") long distanceInMinutes) {
-
-        stationService.addEdge(stationName1, stationName2, distanceInMinutes);
-
-        ModelAndView modelAndView = new ModelAndView("addEdge");
-        List<StationConnect> allEdges = stationService.getAllEdges();
-        modelAndView.addObject("edgeList", allEdges);
-        return modelAndView;
-
-    }
 }
--- src/main/java/com/ivolodin/controller/StationController.java
+++ src/main/java/com/ivolodin/controller/StationController.java
@@ -1,8 +1,66 @@
 package com.ivolodin.controller;
 
+import com.ivolodin.entities.Station;
+import com.ivolodin.entities.StationConnect;
+import com.ivolodin.service.StationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class StationController {
+    private final StationService stationService;
 
+    @Autowired
+    public StationController(StationService stationService) {
+        this.stationService = stationService;
+    }
+
+    @GetMapping("/addStation")
+    public ModelAndView showStationList() {
+
+        ModelAndView modelAndView = new ModelAndView("addStation");
+        List<Station> allStations = stationService.getAllStations();
+        modelAndView.addObject("stationList", allStations);
+
+        return modelAndView;
+    }
+
+    @PostMapping("/addStation")
+    public ModelAndView addStation(@RequestParam(name = "stationName") String stationName) {
+        stationService.addStation(stationName);
+
+        ModelAndView modelAndView = new ModelAndView("addStation");
+        List<Station> allStations = stationService.getAllStations();
+        modelAndView.addObject("stationList", allStations);
+
+        return modelAndView;
+    }
+
+    @GetMapping("/addEdge")
+    public ModelAndView showEdgeList() {
+        ModelAndView modelAndView = new ModelAndView("addEdge");
+        List<StationConnect> allEdges = stationService.getAllEdges();
+        modelAndView.addObject("edgeList", allEdges);
+        return modelAndView;
+    }
+
+    @PostMapping("/addEdge")
+    public ModelAndView addEdge(@RequestParam(name = "stationName1") String stationName1,
+                                @RequestParam(name = "stationName2") String stationName2,
+                                @RequestParam(name = "distance") long distanceInMinutes) {
+
+        stationService.addEdge(stationName1, stationName2, distanceInMinutes);
+
+        ModelAndView modelAndView = new ModelAndView("addEdge");
+        List<StationConnect> allEdges = stationService.getAllEdges();
+        modelAndView.addObject("edgeList", allEdges);
+        return modelAndView;
+
+    }
 }
